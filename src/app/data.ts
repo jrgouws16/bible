229ad81d09184edf024b4aa4d1156ea5ff559d27
jrgouws16 <johangouws16@gmail.com>
2022-06-07 08:03:02 +0200

    1 file modified

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,34 +1,75 @@
+export class Character {
+  name;
+  description;
+  children;
+
+  constructor(name, description, children) {
+    this.name = name || 'Invalid name';
+    this.description = description || 'No description';
+    this.children = children || [];
+  }
+}
+
 export const data = {
   name: 'Adam en Eva',
-  description: 'Adam en Eva (Havah) word op die 6de dag geskape. ',
+  description: 'Adam en Eva (Havah) word op die 6de dag geskape.',
   children: [
     {
-      name: 'analytics',
-      description: '',
+      name: 'Kain',
+      description:
+        'Beroep was n landbouer. Hy het vir Abel doodgeslaan oor hy jaloers was dat die Here Abel se offergawes aanvaar het maar nie syne nie.',
       children: [
         {
-          name: 'cluster',
+          name: 'Henog',
           description: '',
           children: [
             {
-              name: 'AgglomerativeCluster',
+              name: 'Irad',
               description: '',
-            },
-            {
-              name: 'CommunityStructure',
-              description: '',
-            },
-            {
-              name: 'HierarchicalCluster',
-              description: '',
-            },
-            {
-              name: 'MergeEdge',
-              description: '',
+              children: [
+                {
+                  name: 'Megujael',
+                  description: '',
+                  children: [
+                    {
+                      name: 'Metusael',
+                      description: '',
+                      children: [
+                        {
+                          name: 'Lameg',
+                          description: '',
+                          children: [
+                            {
+                              name: 'Jabal',
+                              description: '',
+                              children: [],
+                            },
+                            {
+                              name: 'Jubal',
+                              description: '',
+                              children: [],
+                            },
+                            {
+                              name: 'Tubal-Kain',
+                              description: '',
+                              children: [],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
             },
           ],
         },
       ],
+    },
+    {
+      name: 'Abel',
+      description: 'Beroep was n skaapwagter. Vermoor deur sy broer Kain.',
+      children: [],
     },
   ],
 };
